b106e6499464212d0f0d94226363085ac74d04a8
Garrett Young <garrettyoung510@gmail.com>
2019-09-23 22:16:59 -0700

    calculates score

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-const correctAnswers = ['D', 'C', 'D', 'D'];
-const form = document.querySelector('.quiz-form');
+const correctAnswers = ["D", "C", "D", "D"];
+const form = document.querySelector(".quiz-form");
+const result = document.querySelector(".result");
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
-})
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  let score = 0;
+  const userAnswers = [form.q1, form.q2.value, form.q3.value, form.q4.value];
+
+  // check answers
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswers[index]) {
+      score += 25;
+    }
+  });
+
+  // show result on page
+  result.querySelector("span").textContent = `${score}%`;
+  //   use classList to remove d-none
+  result.classList.remove("d-none");
+});
