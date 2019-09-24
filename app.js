6e409efd30e459db512ffc4bea3802c4a6e974bf
Garrett Young <garrettyoung510@gmail.com>
2019-09-24 14:20:30 -0700

    updated calculations depending on # of questions and answers as well as the wording to answers of questions

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ form.addEventListener("submit", e => {
   e.preventDefault();
 
   let score = 0;
-  const userAnswers = [form.q1, form.q2.value, form.q3.value, form.q4.value];
+  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
 
   // check answers
   userAnswers.forEach((answer, index) => {
     if (answer === correctAnswers[index]) {
-      score += 25;
+      score += 100/correctAnswers.length;
     }
   });
 
