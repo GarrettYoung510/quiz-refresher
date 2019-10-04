3eaad5e61ddd3643c540661f1c88800c2ce41d15
Garrett Young <GarrettYoung510@Gmail.com>
2019-10-03 19:46:15 -0700

    added one question

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const correctAnswers = ["D", "C", "D", "D"];
+const correctAnswers = ["D", "C", "D", "D", "A"];
 const form = document.querySelector(".quiz-form");
 const result = document.querySelector(".result");
 
@@ -6,12 +6,30 @@ form.addEventListener("submit", e => {
   e.preventDefault();
 
   let score = 0;
-  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value];
+
+  // // iterate through to add a number for each question in the app
+  // const answerNums = [];
+  // for (let i = 1; i <= correctAnswers.length; i++) {
+  //   answerNums.push("q" + i);
+  // }
+  // // add all user answers
+  // const userAnswers = [];
+  // // loop through values to check answer values
+  // for (let i = 1; i <= answerNums.length; i++) {
+  //   userAnswers.push(form.answerNums[i].value);
+  //   console.log(userAnswers);
+  // }
+
+  // for (let i = 0; i < correctAnswers.length; i++) {
+  //   userAnswers.push(form.elements[i].value);
+  //   console.log(userAnswers);
+  // }
 
   // check answers
   userAnswers.forEach((answer, index) => {
     if (answer === correctAnswers[index]) {
-      score += 100/correctAnswers.length;
+      score += 100 / correctAnswers.length;
     }
   });
 
@@ -22,18 +40,17 @@ form.addEventListener("submit", e => {
   //   use classList to remove d-none
   result.classList.remove("d-none");
 
-//   animation effect 
+  //   animation effect
   let output = 0;
   const timer = setInterval(() => {
-      result.querySelector('span').textContent = `${output}%`;
-      if(output === score) {
-          clearInterval(timer);
-      } else {
-          output++;
-      }
+    result.querySelector("span").textContent = `${output}%`;
+    if (output === score) {
+      clearInterval(timer);
+    } else {
+      output++;
+    }
   }, 10);
 });
-
 
 // // executes ONCE after 3000 ms
 // setTimeout(() => {
